b0a6a684e8675d4d4285f9daa85846d28f2a6cfe
Aleksandr Tarasenko <work.tarasenko@gmail.com>
2014-07-17 15:34:42 +0300

    added demonstration of work of decorator for pizza with cheese

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -1,4 +1,5 @@
 #include "PizzaBase.h"
+#include "Cheese.h"
 #include <vector>
 #include <memory>
 #include <iostream>
@@ -11,6 +12,9 @@ int main()
     VectorOfPizzas pizzas;
     PointerToPizza base_pizza(new Pizza::PizzaBase());
     pizzas.push_back(base_pizza);
+    std::shared_ptr<Pizza::Cheese> pizza_with_cheese(
+        new Pizza::Cheese(PointerToPizza(new Pizza::PizzaBase)));
+    pizzas.push_back(pizza_with_cheese);
     
     for(VectorOfPizzas::iterator pizza_iterator = pizzas.begin();
         pizza_iterator != pizzas.end();
